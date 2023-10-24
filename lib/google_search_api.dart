6df03492abc10d4e6d67cb69eae771a6a46345a2
Azaz <azazwinner786@gmail.com>
2023-10-25 02:22:52 +0530

    1commit

--- lib/google_search_api.dart
+++ lib/google_search_api.dart
@@ -38,7 +38,7 @@ class _GoogleSearchPlacesApiState extends State<GoogleSearchPlacesApi> {
   void getSuggestion(String input) async {
 
 
-    String kPLACES_API_KEY = "your api key for new Branch1";
+    String kPLACES_API_KEY = "your api key for new Branch1 first commit";
     String type = '(regions)';
 
     try{
