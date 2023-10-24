ea0fc5a36f222fb59fbc1257d8c908f5d719958e
Azaz <azazwinner786@gmail.com>
2023-10-24 23:52:08 +0530

    secondcommit

--- lib/google_search_api.dart
+++ lib/google_search_api.dart
@@ -38,7 +38,7 @@ class _GoogleSearchPlacesApiState extends State<GoogleSearchPlacesApi> {
   void getSuggestion(String input) async {
 
 
-    String kPLACES_API_KEY = "your api key";
+    String kPLACES_API_KEY = "your api key second";
     String type = '(regions)';
 
     try{
